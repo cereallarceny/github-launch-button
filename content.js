fd6cca097454c6f0b205d23a2500719b57d9d226
cereallarceny <me@patrickcason.com>
2019-03-20 17:42:32 -0300

    On the second button

--- content.js
+++ content.js
@@ -1,16 +1,23 @@
-var mergeMessage = document.querySelector(".merge-message");
-var altMergeOptions = mergeMessage.querySelector(".alt-merge-options");
-var selectMenuButton = mergeMessage.querySelector(".select-menu-button");
-var mergeButton = mergeMessage.querySelector(".btn");
+var mergePullRequestForm = document.querySelector("form.js-merge-pull-request");
+var buttonsParent = mergePullRequestForm.querySelector(
+  ".commit-form-actions .select-menu"
+);
+var cancelButton = mergePullRequestForm.querySelector(".btn.js-details-target");
+var mergeButton = mergePullRequestForm.querySelector(
+  ".btn.js-merge-commit-button"
+);
 
 // Only do this if it's a clean merge
 if (mergeButton.classList.contains("btn-primary")) {
-  // Hide the things around the button
-  altMergeOptions.style.display = "none";
-  selectMenuButton.style.display = "none";
+  // Set parent styles
+  buttonsParent.style.position = "relative";
+  buttonsParent.style.width = "100%";
+  buttonsParent.style.height = "400px";
 
-  mergeMessage.style.position = "relative";
-  mergeMessage.style.height = "400px";
+  // Center the cancel button
+  cancelButton.style.marginLeft = "auto";
+  cancelButton.style.marginRight = "auto";
+  cancelButton.style.display = "block";
 
   // Change the merge button to have a special ID which overrides all other styles
   mergeButton.id = "special-merge-button";
